760bafc94d6f9773e23ac62582a316573b9dcbb4
didier amyot <didier.amyot@gmail.com>
2017-10-25 22:20:07 -0400

    bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-jupyter "0.1.2"
+(defproject lein-jupyter "0.1.3"
   :description "Leiningen plugin for jupyter notebook."
   :url "https://github.com/didiercrunch/lein-jupyter"
   :license {:name " MIT License"}
