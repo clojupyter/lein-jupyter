0c7af82b8b7eaff85a499858664fcef134d2aff3
didier amyot <didier.amyot@gmail.com>
2017-12-16 10:18:04 -0500

    upgraded version of clojupyter to support utf-8 strings.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject lein-jupyter "0.1.10"
+(defproject lein-jupyter "0.1.11"
   :description "Leiningen plugin for jupyter notebook."
   :url "https://github.com/didiercrunch/lein-jupyter"
   :license {:name " MIT License"}
-  :dependencies [[org.clojars.didiercrunch/clojupyter "0.1.0"]
+  :dependencies [[org.clojars.didiercrunch/clojupyter "0.1.1"]
                  [cheshire "5.8.0"]
                  [org.apache.commons/commons-exec "1.3"]]
   :resource-paths ["resources"]
