8ad9d52d6d3b85734cc0f6e7f77edb9af105f632
Dave Dixon <dave.d.dixon@gmail.com>
2018-03-10 08:53:54 -0800

    Correct :url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-jupyter "0.1.15"
   :description "Leiningen plugin for jupyter notebook."
-  :url "https://github.com/provisdom/lein-jupyter"
+  :url "https://github.com/clojupyter/lein-jupyter"
   :license {:name " MIT License"}
   :dependencies [[org.clojars.didiercrunch/clojupyter "0.1.3"]  ;; this dependency needs to be
                                                                 ;; updated in leiningen.jupyter.kernel
