5347c1af651780bd35e9d63ba5bb48c335a0738d
didier amyot <didier.amyot@gmail.com>
2018-03-10 14:48:15 -0500

    bumped clojupyter version

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject lein-jupyter "0.1.15"
+(defproject lein-jupyter "0.1.16"
   :description "Leiningen plugin for jupyter notebook."
   :url "https://github.com/clojupyter/lein-jupyter"
   :license {:name " MIT License"}
-  :dependencies [[org.clojars.didiercrunch/clojupyter "0.1.3"]  ;; this dependency needs to be
+  :dependencies [[org.clojars.didiercrunch/clojupyter "0.1.5"]  ;; this dependency needs to be
                                                                 ;; updated in leiningen.jupyter.kernel
                                                                 ;; manually.
                  [cheshire "5.8.0"]
