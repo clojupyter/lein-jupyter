a4aee79b47e6d6956a863153a755c6eedfdf8b01
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2019-07-23 10:54:57 -0300

    Add Nubank modifications to project.clj, support Clojupyter 0.2.2

--- project.clj
+++ project.clj
@@ -1,11 +1,15 @@
-(defproject lein-jupyter "0.1.16"
+(defproject nubank/lein-jupyter "0.1.17"
   :description "Leiningen plugin for jupyter notebook."
-  :url "https://github.com/clojupyter/lein-jupyter"
-  :license {:name " MIT License"}
-  :dependencies [[org.clojars.didiercrunch/clojupyter "0.1.5"]  ;; this dependency needs to be
-                                                                ;; updated in leiningen.jupyter.kernel
-                                                                ;; manually.
-                 [cheshire "5.8.0"]
+  :url "https://github.com/nubank/lein-jupyter"
+  :license {:name "MIT License"}
+
+  :repositories [["central"  {:url "https://repo1.maven.org/maven2/" :snapshots false}]
+                 ["clojars"  {:url "https://clojars.org/repo/"}]]
+
+  :dependencies [[nubank/clojupyter "0.2.2"] ;; this dependency needs to be
+                                             ;; updated in leiningen.jupyter.kernel
+                                             ;; manually.
+                 [cheshire "5.8.1"]
                  [org.apache.commons/commons-exec "1.3"]]
   :resource-paths ["resources"]
   :eval-in-leiningen true)
