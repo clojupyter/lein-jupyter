3eae44d4fef4be9ab825c88a911406b219b755bf
Nicolas Modrzyk <hellonico@gmail.com>
2020-01-31 13:26:49 +0900

    update clojupyter

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject lein-jupyter "0.1.16"
+(defproject lein-jupyter "0.1.17"
   :description "Leiningen plugin for jupyter notebook."
   :url "https://github.com/clojupyter/lein-jupyter"
   :license {:name " MIT License"}
-  :dependencies [[org.clojars.didiercrunch/clojupyter "0.1.5"]  ;; this dependency needs to be
+  :dependencies [[clojupyter "0.3.1"]  ;; this dependency needs to be
                                                                 ;; updated in leiningen.jupyter.kernel
                                                                 ;; manually.
                  [cheshire "5.8.0"]
