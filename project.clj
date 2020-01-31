9f29fe0b0b88057c3558f3397fbb87535300d0c1
Nicolas Modrzyk <hellonico@gmail.com>
2020-01-31 13:47:59 +0900

    add release steps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-jupyter "0.1.17"
+(defproject hellonico/lein-jupyter "0.1.17"
   :description "Leiningen plugin for jupyter notebook."
   :url "https://github.com/clojupyter/lein-jupyter"
   :license {:name " MIT License"}
@@ -8,4 +8,14 @@
                  [cheshire "5.8.0"]
                  [org.apache.commons/commons-exec "1.3"]]
   :resource-paths ["resources"]
+   :release-tasks 
+    [["vcs" "assert-committed"]
+     ["change" "version" "leiningen.release/bump-version" "release"]
+     ["vcs" "commit"]
+     ["vcs" "tag" "--no-sign"]
+     ; ["deploy" "vendredi"]
+     ["deploy" "clojars"]
+     ["change" "version" "leiningen.release/bump-version"]
+     ["vcs" "commit"]
+     ["vcs" "push"]]
   :eval-in-leiningen true)
