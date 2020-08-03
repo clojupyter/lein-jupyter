7e95c6b8ca4d349c8738b995a23255d17b58485f
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2020-08-03 16:08:16 -0300

    Downgrade cheshire
    Use ther same version (5.8.1) as used in clojupyter 0.3.2, to avoid
    strange issues when resolving dependencies.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nubank/lein-jupyter "0.1.20"
+(defproject nubank/lein-jupyter "0.1.21"
   :description "Leiningen plugin for jupyter notebook."
   :url "https://github.com/nubank/lein-jupyter"
   :license {:name "MIT License"}
@@ -7,7 +7,9 @@
                  ["clojars" {:url "https://clojars.org/repo/"}]]
 
   :dependencies [[clojupyter "0.3.2"]
-                 [cheshire "5.10.0"]
+                 ;; Use the same version as used in clojupyter, or this can result in strange errors
+                 [cheshire "5.8.1"]
                  [org.apache.commons/commons-exec "1.3"]]
+
   :resource-paths ["resources"]
   :eval-in-leiningen true)
