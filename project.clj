7d8b31e8e8fcc1662538a71345c233eefd0709b8
Raphael Negrisoli <raphaelnegrisoli@gmail.com>
2022-04-07 10:32:21 -0300

    Bump clojupyter

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nubank/lein-jupyter "0.1.23"
+(defproject nubank/lein-jupyter "0.1.24"
   :description "Leiningen plugin for jupyter notebook."
   :url "https://github.com/nubank/lein-jupyter"
   :license {:name "MIT License"}
@@ -8,7 +8,7 @@
 
   ;; TODO: Go back to clojupyter upstream after this PR is merged and a new release
   ;; is made: https://github.com/clojupyter/clojupyter/pull/135
-  :dependencies [[dev.nubank/clojupyter "0.3.2-fix1"]
+  :dependencies [[dev.nubank/clojupyter "0.3.3-SNAPSHOT"]
                  [org.apache.commons/commons-exec "1.3"]]
   :resource-paths ["resources"]
   :eval-in-leiningen true)
